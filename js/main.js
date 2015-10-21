5f5cfb685563452a2a4780bbd5cf3718c7261551
xinnix2000 <xinnix2000@gmail.com>
2015-10-21 14:12:54 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,111 @@
+var lineReader = require('line-reader');
+var nodeExcel  = require('excel-export');
+
+
+var conf = {};
+
+var re = new RegExp(/^[1-9]\d*$/g);
+keywords = ['By:','Source:','Research Fields:','Times Cited:'];
+otherkey = ['ESI Hot','Research Front'];
+
+
+  oneInv = {};
+
+var trim = function(str) {
+    //return this.replace(/[(^\s+)(\s+$)]/g,"");//會把字符串中間的空白符也去掉
+    //return this.replace(/^\s+|\s+$/g,""); //
+    return str.replace(/^\s+/g,"").replace(/\s+$/g,"");
+}
+
+function parserLine(line){
+	line = trim(line);
+	if(line.length!=0){
+		var res = line.match(re);
+		if(res){
+			oneInv['order']=res[0];
+			return;
+		}
+		for(key in keywords){
+			var pos = line.indexOf(keywords[key]);
+			// console.log('keywords:'+key);
+			if(pos!=-1){
+				oneInv[keywords[key]]= line.substring(pos+keywords[key].length);
+				return;
+			}
+		}
+
+		for(key in otherkey){
+			var pos = line.indexOf(otherkey[key]);
+			// console.log('keywords:'+key);
+			if(pos!=-1){
+				return;
+			}
+		}
+
+		oneInv['title'] = line;
+	}
+
+}
+var count = 0;
+function readfile(name){
+	lineReader.eachLine(name, function(line, last) {
+
+		// console.log(line);
+		// console.log(count++);
+  		parserLine(line);
+		// console.log(Object.getOwnPropertyNames(oneInv).length);
+  		if(Object.getOwnPropertyNames(oneInv).length ==  6){
+  			conf.rows .push(oneInv);
+  			console.log(conf.rows);
+  			return
+  		}
+  		
+		
+ 	 	if (/* done */last) {
+   		 	return false; // stop reading
+  		}
+	});
+}
+
+readfile('a.txt');
+
+
+
+// a = 'Times Cited: 840';
+// parserLine(a);
+
+// console.log(oneInv);
+
+
+
+conf.cols = [{
+	caption:'order',
+	type:'string'
+},{
+	caption:'Title',
+	type:'string',
+},{
+	caption:'Author',
+	type:'string'
+},{
+	caption:'Source',
+	type:'string'
+},{
+	caption:'Research Fields',
+	type:'string'
+},{
+	caption:'Times Cited',
+	type:'string' 
+}
+];
+conf.rows = [
+     
+    ];
+
+// var result = nodeExcel.execute(conf);
+
+// var fs=require("fs");  
+// fs.writeFile('./1.xslx',result,'binary',function(err){
+// 	if(err) throw err;
+// 	console.log('complete');
+// });
